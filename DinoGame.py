f471ffc8987f3cfba98572b4fbc7068b392a4662
farya <fdash@yandex.ru>
2023-11-24 21:17:55 +0300

    Add display

--- DinoGame.py
+++ DinoGame.py
@@ -2,6 +2,15 @@ import pygame
 
 pygame.init()
 
+display_width = 800
+display_height = 600
+
+display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Run, Dino!')
+
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
 
 def run_game():
     game = True
@@ -11,6 +20,9 @@ def run_game():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+        display.fill((255, 255, 255))
+        pygame.display.update()
 
 
 run_game()
